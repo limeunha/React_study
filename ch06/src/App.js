7500ed72ebd670c7d3819e0a4488a7890a2b25da
limeunha <eh1501@naver.com>
2024-11-01 13:23:01 +0900

    수정작업

--- ch06/src/App.js
+++ ch06/src/App.js
@@ -1,7 +1,9 @@
 import UseEffectEx from './UseEffectEX'
+import UseMemoEx1 from './UseMemoEx1'
 
 function App() {
-   return <UseEffectEx />
+   // return <UseEffectEx />
+   return <UseMemoEx1 />
 }
 
 export default App
